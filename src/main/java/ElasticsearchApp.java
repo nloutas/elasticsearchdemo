957819265bbedc22e4a337ebae6b755a466a601b
nloutas <nloutas@gmail.com>
2015-11-17 15:17:35 +0100

    enhanced test entry

--- src/main/java/ElasticsearchApp.java
+++ src/main/java/ElasticsearchApp.java
@@ -1,8 +1,8 @@
+
+import org.joda.time.DateTime;
 import java.net.UnknownHostException;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.UUID;
-
 
 
 /**
@@ -23,16 +23,35 @@ public class ElasticsearchApp {
     }
 
     HashMap<String, String> entry = new HashMap<String, String>();
-    entry.put("Teid", "1");
-    entry.put("UsageData", "100Kb");
-    entry.put("Cost", "0.1");
-    entry.put("Currency", "Euro");
-    entry.put("TrafficType", "RX");
-    entry.put("Imsi", "2200000001");
-    entry.put("Organisation", "Enterprise 11");
-    entry.put("Sender", "Vodafone DE");
-    entry.put("Recipient", "EPlus");
-    entry.put("Timestamp", (new Date()).toString());
+    entry.put("teid", "1");
+    entry.put("endpoint_id", "932");
+    entry.put("endpoint_name", "My Endpoint");
+    entry.put("sim_id", "123");
+    entry.put("iccid", "0527242926180300002");
+    entry.put("imsi_id", "321");
+    entry.put("imsi", "901430000000105");
+    entry.put("cost", "0.1");
+    entry.put("currency_id", "1");
+    entry.put("currency", "EUR");
+    entry.put("organisation_id", "123");
+    entry.put("organisation_name", "Enterprise 11");
+    entry.put("Sender", "XXANY");
+    entry.put("Recipient", "EMNDE");
+    entry.put("event_start_timestamp", new DateTime().minusMillis(10000).toDate().toString());
+    entry.put("event_stop_timestamp", new DateTime().toDate().toString());
+    entry.put("tariff_id", "302");
+    entry.put("tariff_name", "Professional");
+    entry.put("ratezone_id", "123");
+    entry.put("ratezone_name", "EU+");
+    entry.put("operator_id", "2");
+    entry.put("operator_name", "ANY");
+    entry.put("country_id", "2");
+    entry.put("country_name", "ANY");
+    entry.put("traffic_type_id", "1");
+    entry.put("traffic_type", "Data");
+    entry.put("volume", "0.04284");
+    entry.put("volume_tx", "0.02142");
+    entry.put("volume_rx", "0.02142");
 
     ec.indexEntry(UUID.randomUUID().toString(), entry);
 
